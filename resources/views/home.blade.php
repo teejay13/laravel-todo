d11ba41e3047f6ca9ea6f2fc93b9c82b2b205447
teejay13 <adetunjisalako@gmail.com>
2017-04-11 00:45:14 +0000

    legoo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="text-center">
+        <h1>Welcome to Laravel ToDo App</h1>
+        <hr/>
+
+        @include('partials.flash_notification')
+
+    </div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<hr/>
+
+<div class="container">
+    &copy; {{ date('Y') }}, <a href="">Copyright Teejay - All Rights Reserved</a>
+    <br/>
+</div>
